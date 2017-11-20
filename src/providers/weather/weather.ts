bbdd9e9e80683678b9139d53e11dc63b366159be
bryanda <bryanda.garcia@gmail.com>
2017-11-20 02:49:08 -0600

    iweather app

---
+++ src/providers/weather/weather.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the WeatherProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class WeatherProvider {
+  apiKey = '3e73a50ad833e958';
+  url;
+
+  constructor(public http: HttpClient) {
+    console.log('Hello WeatherProvider Provider');
+    this.url = 'http://api.wunderground.com/api/'+this.apiKey+'/conditions/q';
+  }
+
+  getWeather(city, state){
+    return this.http.get(this.url+'/'+state+'/'+city+'.json');
+    /*
+    removed map(res => res.json())
+    because I read it's no longer needed and
+    I was getting an error that res.json is not a function
+    */
+  }
+
+}
